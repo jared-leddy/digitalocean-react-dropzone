3af72116bf20133462a4aa5e5886c3b0e068aa9d
Jared Ledbetter <57743183+jared-leddy@users.noreply.github.com>
2022-03-01 15:08:17 -0500

    migrate files from spfx-test repo

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// https://www.digitalocean.com/community/tutorials/react-react-dropzone
+
+import DropzoneComponent from "./DropzoneComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <DropzoneComponent />
+    </div>
+  );
+}
+
+export default App;
